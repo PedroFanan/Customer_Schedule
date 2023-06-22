87bcdc168b8a9656ea0726073b1850efa18b42aa
Pedro Fanan <120430759+PedroFanan@users.noreply.github.com>
2023-06-21 22:23:35 -0300

    first commit

---
+++ src/Model/Cliente.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author pedro.fanan
+ */
+public class Cliente extends Pessoa {
+
+    private String endereco;
+    private String cep;
+
+    public Cliente(int id, String nome, char sexo, String dataNascimento, String telefone, String email, String rg, String endereco, String cep) {
+        super(id, nome, sexo, dataNascimento, telefone, email, rg);
+        this.endereco = endereco;
+        this.cep = cep;
+    }
+
+    public Cliente(String endereco, String cep, int id, String nome) {
+        super(id, nome);
+        this.endereco = endereco;
+        this.cep = cep;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    @Override
+    public String toString() {
+        return getNome();
+    }
+
+}
---
+++ src/Service/EmailTeste.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+/**
+ *
+ * @author pedro.fanan
+ */
+public class EmailTeste {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+
+        Email email = new Email(
+               "Assunto da Barbearia", 
+                "Mensagem padrão do Sistema da Barbearia\n" + "Usuario:FelipeSoster\n" + "Senha: 123456", 
+               "tiagoluz.h607@gmail.com");
+
+       email.enviar();
+	}
+
+}
